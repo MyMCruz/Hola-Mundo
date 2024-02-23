8646be541777ffac5d2a92891440efdba7ed34e7
MyMCruz <144400744+MyMCruz@users.noreply.github.com>
2024-02-23 02:09:11 +0000

    Version 2

---
+++ Include/Mascota.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include<Alimento.hpp>
+class Macota
+{
+    private:
+        int vida;
+    public:
+        Mascota()
+        {
+            this -> vida=0
+        }
+            ~Mascota(){}
+            void Comer(Alimento alimento)
+        {
+        }
+
+}
